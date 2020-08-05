ca1113a4986aad1eee7559b9047dc593d013b8ea
Noblepal <zeronillzero@gmail.com>
2020-08-05 14:16:30 +0300

    SharedPrefsManager

--- app/src/main/java/com/icelabs/codesnippets/util/SharedPrefsManager.java
+++ app/src/main/java/com/icelabs/codesnippets/util/SharedPrefsManager.java
@@ -4,12 +4,9 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 public class SharedPrefsManager {
-    private static final String KEY_USER_DATA = "key_user_data";
-    private static final String KEY_IS_USER_REGISTERED = "key_is_user_registered";
-    private static final String KEY_CACHED_TAGS = "key_cached_tags";
     private static SharedPreferences sharedPreferences;
     private static SharedPrefsManager mInstance;
-    private final String SHARED_PREFS_NAME = "kenya_sihami";
+    private final String SHARED_PREFS_NAME = "shared_prefs_name";
     private static final String TAG = "SharedPrefsManager";
 
     private SharedPrefsManager(Context context) {
